0ebc886c862ef14dbf632023d7bd9af81f8d5e7c
hnpyn <wangmingne@outlook.com>
2023-06-02 16:37:29 +0800

    update tmux

--- tmux/clean_plugins.sh
+++ tmux/clean_plugins.sh
@@ -2,6 +2,8 @@
 echo "clean plugins..."
 rm -rf ./plugins
 mkdir plugins
+echo "done!"
 # use tpm
 echo "getting tpm..."
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+echo "finished."
