b9bca654563a9d5d71561053a0afca884990cb56
hnpyn <wangmingne@outlook.com>
2023-06-05 14:46:16 +0800

    comment clang_format

--- nvim/lua/plugins/nullls.lua
+++ nvim/lua/plugins/nullls.lua
@@ -10,7 +10,7 @@ return {
 				sources = {
 					null_ls.builtins.formatting.stylua,
 					null_ls.builtins.formatting.black,
-					null_ls.builtins.formatting.clang_format,
+					-- null_ls.builtins.formatting.clang_format,
 					null_ls.builtins.formatting.markdownlint,
 				},
 				on_attach = function(client, bufnr)
