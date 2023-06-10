931750699dc9f713113824cf2359a0a5854b06e1
hnpyn <wangmingne@outlook.com>
2023-06-10 17:22:47 +0800

    dap keymaps

--- nvim/lua/daprc/dapmap.lua
+++ nvim/lua/daprc/dapmap.lua
@@ -1,22 +1,24 @@
 -- dap ui keybindings
 local map = vim.keymap.set
-local opt = { noremap = true, silent = true }
+local function opts(desc)
+	return { desc = "Dap: " .. desc, noremap = true, silent = true }
+end
 
 map("n", ",c", function()
 	require("dap").continue()
-end, { desc = "dap continue" })
+end, opts("continue"))
 map("n", ",b", function()
 	require("dap").toggle_breakpoint()
-end, { desc = "dap set breakpoint" })
+end, opts("set breakpoint"))
 map("n", ",n", function()
 	require("dap").step_over()
-end, { desc = "dap step over" })
+end, opts("step over"))
 map("n", ",i", function()
 	require("dap").step_into()
-end, { desc = "dap setp into" })
+end, opts("setp into"))
 map("n", ",o", function()
 	require("dap").step_out()
-end, { desc = "dap step out" })
+end, opts("step out"))
 map("n", ",q", function()
 	require("dap").disconnect()
-end, { desc = "dap disconnect" })
+end, opts("disconnect"))
