bbff822709302fb71ed9f34bde56b3028b4fd767
hnpyn <wangmingne@outlook.com>
2023-07-04 14:37:31 +0800

    nvim toggleterm

--- config/nvim/lua/plugins/toggleterm.lua
+++ config/nvim/lua/plugins/toggleterm.lua
@@ -1,34 +1,36 @@
 return {
-  {
-    "akinsho/toggleterm.nvim",
-    version = "*",
-    config = function()
-      require("toggleterm").setup({})
+	{
+		"akinsho/toggleterm.nvim",
+		version = "*",
+		config = function()
+			require("toggleterm").setup({})
 
-      local function opts(desc)
-        return { desc = "" .. desc, noremap = true, silent = true }
-      end
+			local function opts(desc)
+				return { desc = "" .. desc, noremap = true, silent = true }
+			end
 
-      -- lazygit
-      local Terminal = require("toggleterm.terminal").Terminal
-      local lazygit = Terminal:new({
-        cmd = "lazygit",
-        dir = "git_dir",
-        direction = "float",
-        float_opts = {
-          border = "curved",
-        },
-      })
+			-- lazygit
+			local Terminal = require("toggleterm.terminal").Terminal
+			local lazygit = Terminal:new({
+				cmd = "lazygit",
+				dir = "git_dir",
+				direction = "float",
+				float_opts = {
+					border = "curved",
+					width = 160,
+					height = 40,
+				},
+			})
 
-      function _lazygit_toggle()
-        lazygit:toggle()
-      end
+			function _lazygit_toggle()
+				lazygit:toggle()
+			end
 
-      -- keymaps
-      vim.keymap.set("n", "<C-t>", "<Cmd>ToggleTerm size=40 direction=float<CR>", opts(""))
-      vim.keymap.set("t", "<C-t>", "<C-\\><C-n><Cmd>q<CR>", opts(""))
-      vim.keymap.set("n", "<Leader>lg", "<Cmd>lua _lazygit_toggle()<CR>", opts("Toggleterm: lazygit"))
-      vim.cmd.cnoreabbrev("lg lua _lazygit_toggle()")
-    end,
-  },
+			-- keymaps
+			vim.keymap.set("n", "<C-t>", "<Cmd>ToggleTerm size=40 direction=float<CR>", opts(""))
+			vim.keymap.set("t", "<C-t>", "<C-\\><C-n><Cmd>q<CR>", opts(""))
+			vim.keymap.set("n", "<Leader>lg", "<Cmd>lua _lazygit_toggle()<CR>", opts("Toggleterm: lazygit"))
+			vim.cmd.cnoreabbrev("lg lua _lazygit_toggle()")
+		end,
+	},
 }
