c208da4bff458a62e03cd1995fec177894671142
hnpyn <wangmingne@outlook.com>
2023-07-11 23:18:54 +0800

    ranger download plugins

--- config/ranger/download_plugins.sh
+++ config/ranger/download_plugins.sh
@@ -1,11 +1,19 @@
 # clean up plugins
-echo "clean plugins..."
+echo ""
+echo " clean plugins..."
+echo ""
+
 rm -rf plugins
 mkdir plugins
 cd plugins
-echo "done!"
-echo "Install plugins..."
+
+echo " done!"
+
+echo ""
+echo " Install plugins..."
 git clone https://github.com/alexanderjeurissen/ranger_devicons
 git clone https://github.com/fdw/ranger-autojump.git
 cp ./ranger-autojump/autojump.py ./autojump.py
-echo "finished."
+
+echo ""
+echo " finished."
