2121aa3ce15cdc9adfbf3ff0da435c3b81579535
hnpyn <wangmingne@outlook.com>
2023-07-14 16:21:10 +0800

    zsh plugins

--- zsh/download_plugins.sh
+++ zsh/download_plugins.sh
@@ -11,7 +11,14 @@ echo ''
 rm -rf $ZSH/plugins
 mkdir -p $ZSH/plugins
 
-git clone https://github.com/zdharma-continuum/fast-syntax-highlighting $ZSH/plugins
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting $ZSH/plugins/fast-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH/plugins/zsh-autosuggestions
+git clone https://github.com/jeffreytse/zsh-vi-mode.git $ZSH/plugins/zsh-vi-mode
+git clone https://github.com/wting/autojump.git $ZSH/plugins/autojump
+
+cd $ZSH/plugins/autojump
+./install.py
+cd $ZSH
 
 echo ''
 echo ' Finished!'
