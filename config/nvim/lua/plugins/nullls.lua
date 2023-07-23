28caacce0df5652074912472f75372433a88083a
hnpyn <wangmingne@outlook.com>
2023-07-23 19:14:13 +0800

    update zsh

--- config/nvim/lua/plugins/nullls.lua
+++ config/nvim/lua/plugins/nullls.lua
@@ -15,6 +15,7 @@ return {
 					null_ls.builtins.formatting.clang_format,
 					null_ls.builtins.formatting.markdownlint,
 					null_ls.builtins.formatting.yamlfmt,
+					null_ls.builtins.formatting.shfmt,
 				},
 				on_attach = function(client, bufnr)
 					if client.supports_method("textDocument/formatting") then
