563a1f32b87d5dfc65d0ebc3a93119659f5ea34d
hnpyn <wangmingne@outlook.com>
2023-07-30 22:20:05 +0800

    fzf-tab

--- zsh/fzf/file_preview.sh
+++ zsh/fzf/file_preview.sh
@@ -1,15 +1,13 @@
 #! /usr/bin/env sh
-
 mime=$(file -bL --mime-type "$1")
 category=${mime%%/*}
 if [ -d "$1" ]; then
-    lsd -l --no-user --icon=auto "$1" 2>/dev/null 
+  lsd -A --icon=always "$1" 2>/dev/null 
 elif [ "$category" = text ]; then
-    (bat -p --style numbers --color=always "$1" ) 2>/dev/null | head -1000
-elif [ "$mime" = application/pdf ]; then 
-  pdftotext $1 - | less 
+  (bat -p --style numbers --color=always "$1" ) 2>/dev/null | head -1000
 elif [ "$category" = image ]; then 
-  (~/.zsh/fzf/img_preview $1) 2>/dev/null 
+  # (~/.zsh/fzf/img_preview $1) 2>/dev/null 
+  chafa "$1"
 else 
-    echo $1 is a $category file
+  echo $1 is a $category file
 fi
--- zsh/fzf/fzf-tab.zsh
+++ zsh/fzf/fzf-tab.zsh
@@ -1,7 +1,7 @@
 export RUNEWIDTH_EASTASIAN=0
 # export FZF_PREVIEW_COMMAND='bash /home/charles/.zsh/file_preview.sh {}'
 # export FZF_DEFAULT_OPTS="--height 12 --layout=reverse --history=/home/charles/.zsh/cache/fzfhistory"
-export FZF_DEFAULT_OPTS="--preview 'bash ~/.zsh/fzf/file_preview.sh {}' --height 12 --layout=reverse"
+export FZF_DEFAULT_OPTS="--preview 'bash ~/.zsh/fzf/file_preview.sh {}' --layout=reverse --height=12"
 export FZF_DEFAULT_COMMAND="fd --exclude={.git,.idea,.vscode,.sass-cache,node_modules,build,dist,vendor} --type f"
 
 zstyle ':completion:complete:*:options' sort false
@@ -14,14 +14,12 @@ zstyle ':fzf-tab:complete:(kill|ps):argument-rest' fzf-preview '[ "$group" = "pr
 zstyle ':fzf-tab:complete:(kill|ps):argument-rest' fzf-flags --preview-window=down:3:wrap
 zstyle ':fzf-tab:complete:*:options' fzf-flags --preview-window=down:0:wrap
 zstyle ':fzf-tab:complete:systemctl-*:*' fzf-preview 'SYSTEMD_COLORS=1 systemctl status $word'
-zstyle ':fzf-tab:complete:yay:*' fzf-preview 'yay -Qi $word || yay -Si $word'
-zstyle ':fzf-tab:complete:pacman:*' fzf-preview 'pacman -Qi $word || pacman -Si $word'
 zstyle ':fzf-tab:complete:(\\|)run-help:*' fzf-preview 'run-help $word'
 zstyle ':fzf-tab:complete:(\\|*/|)man:*' fzf-preview 'man $word'
 zstyle ':fzf-tab:complete:git-(add|diff|restore):*' fzf-preview 'git diff --color=always $word'
 zstyle ':fzf-tab:complete:git-log:*' fzf-preview 'git log --color=always $word'
 zstyle ':fzf-tab:complete:git-show:*' fzf-preview 'git show --color=always $word'
 zstyle ':fzf-tab:complete:git-checkout:*' fzf-preview '[ -f "$realpath" ] && git diff --color=always $word || git log --color=always $word'
-zstyle ':fzf-tab:complete:*:*' fzf-preview 'bash ~/.zsh/file_preview.sh ${(Q)realpath}'
+zstyle ':fzf-tab:complete:*:*' fzf-preview 'bash ~/.zsh/fzf/file_preview.sh ${(Q)realpath}'
 zstyle ':fzf-tab:complete:*:*' fzf-flags --height=12
-zstyle ':fzf-tab:complete:tldr:argument-1' fzf-preview 'tldr --color always $word'
+zstyle ':fzf-tab:complete:brew-(install|uninstall|search|info):*-argument-rest' fzf-preview 'brew info $word'
