0104bb26b5f1d0dee8fb12d62e2f511d346c9d31
hnpyn <wangmingne@outlook.com>
2023-08-25 02:40:28 +0800

    nvim plugins

--- config/nvim/lua/plugins/core.lua
+++ config/nvim/lua/plugins/core.lua
@@ -12,12 +12,14 @@ return {
     config = function()
       vim.o.timeout = true
       vim.o.timeoutlen = 1000
-      require("which-key").setup({
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
-      })
+      require("which-key").setup({})
     end,
+  },
+  {
+    "folke/trouble.nvim",
+    event = "VeryLazy",
+    cmd = "Trouble",
+    opts = {},
   },
   {
     "akinsho/toggleterm.nvim",
