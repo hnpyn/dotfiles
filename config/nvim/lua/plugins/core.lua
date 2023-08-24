478d21f5ce71d2fe75d272b2d63da081c34817de
hnpyn <wangmingne@outlook.com>
2023-08-25 02:31:54 +0800

    nvim plugins

--- config/nvim/lua/plugins/core.lua
+++ config/nvim/lua/plugins/core.lua
@@ -1,62 +1,101 @@
 return {
-	{ "nvim-lua/plenary.nvim" },
-	{
-		"windwp/nvim-autopairs",
-		event = "VeryLazy",
-		config = function()
-			require("nvim-autopairs").setup({})
-		end,
-	},
-	{
-		"folke/which-key.nvim",
-		config = function()
-			vim.o.timeout = true
-			vim.o.timeoutlen = 1000
-			require("which-key").setup({
-				-- your configuration comes here
-				-- or leave it empty to use the default settings
-				-- refer to the configuration section below
-			})
-		end,
-	},
-	{
-		"akinsho/toggleterm.nvim",
-		version = "*",
-		config = function()
-			require("toggleterm").setup({})
+  { "nvim-lua/plenary.nvim" },
+  {
+    "windwp/nvim-autopairs",
+    event = "VeryLazy",
+    config = function()
+      require("nvim-autopairs").setup({})
+    end,
+  },
+  {
+    "folke/which-key.nvim",
+    config = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 1000
+      require("which-key").setup({
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      })
+    end,
+  },
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    config = function()
+      require("toggleterm").setup({})
 
-			local function opts(desc)
-				return { desc = "" .. desc, noremap = true, silent = true }
-			end
+      local function opts(desc)
+        return { desc = "" .. desc, noremap = true, silent = true }
+      end
 
-			-- lazygit
-			local Terminal = require("toggleterm.terminal").Terminal
-			local lazygit = Terminal:new({
-				cmd = "lazygit",
-				dir = "git_dir",
-				direction = "float",
-				float_opts = {
-					border = "curved",
-					width = 160,
-					height = 40,
-				},
-			})
+      -- lazygit
+      local Terminal = require("toggleterm.terminal").Terminal
+      local lazygit = Terminal:new({
+        cmd = "lazygit",
+        dir = "git_dir",
+        direction = "float",
+        float_opts = {
+          border = "curved",
+          width = 160,
+          height = 40,
+        },
+      })
 
-			function _lazygit_toggle()
-				lazygit:toggle()
-			end
+      function _lazygit_toggle()
+        lazygit:toggle()
+      end
 
-			-- keymaps
-			vim.keymap.set("n", "<C-t>", "<Cmd>ToggleTerm size=40 direction=float<CR>", opts(""))
-			vim.keymap.set("t", "<C-t>", "<C-\\><C-n><Cmd>q<CR>", opts(""))
-			vim.keymap.set("n", "<Leader>lg", "<Cmd>lua _lazygit_toggle()<CR>", opts("Toggleterm: lazygit"))
-			vim.cmd.cnoreabbrev("lg lua _lazygit_toggle()")
-		end,
-	},
-	{
-		"chrishrb/gx.nvim",
-		event = { "BufEnter" },
-		dependencies = { "nvim-lua/plenary.nvim" },
-		config = true, -- default settings
-	},
+      -- keymaps
+      vim.keymap.set("n", "<C-t>", "<Cmd>ToggleTerm size=40 direction=float<CR>", opts(""))
+      vim.keymap.set("t", "<C-t>", "<C-\\><C-n><Cmd>q<CR>", opts(""))
+      vim.keymap.set("n", "<Leader>lg", "<Cmd>lua _lazygit_toggle()<CR>", opts("Toggleterm: lazygit"))
+      vim.cmd.cnoreabbrev("lg lua _lazygit_toggle()")
+    end,
+  },
+  {
+    "chrishrb/gx.nvim",
+    event = { "BufEnter" },
+    config = true, -- default settings
+  },
+  {
+    "kevinhwang91/nvim-ufo",
+    enabled = false,
+    dependencies = { "kevinhwang91/promise-async" },
+    config = function()
+      local handler = function(virtText, lnum, endLnum, width, truncate)
+        local newVirtText = {}
+        local suffix = (' 󰁂 %d '):format(endLnum - lnum)
+        local sufWidth = vim.fn.strdisplaywidth(suffix)
+        local targetWidth = width - sufWidth
+        local curWidth = 0
+        for _, chunk in ipairs(virtText) do
+          local chunkText = chunk[1]
+          local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+          if targetWidth > curWidth + chunkWidth then
+            table.insert(newVirtText, chunk)
+          else
+            chunkText = truncate(chunkText, targetWidth - curWidth)
+            local hlGroup = chunk[2]
+            table.insert(newVirtText, { chunkText, hlGroup })
+            chunkWidth = vim.fn.strdisplaywidth(chunkText)
+            -- str width returned from truncate() may less than 2nd argument, need padding
+            if curWidth + chunkWidth < targetWidth then
+              suffix = suffix .. (' '):rep(targetWidth - curWidth - chunkWidth)
+            end
+            break
+          end
+          curWidth = curWidth + chunkWidth
+        end
+        table.insert(newVirtText, { suffix, 'MoreMsg' })
+        return newVirtText
+      end
+      require("ufo").setup({
+        fold_virt_text_handler = handler,
+        provider_selector = function(bufnr, filetype, buftype)
+          return { "treesitter", "indent" }
+        end
+      })
+    end,
+  },
 }
--- config/nvim/lua/plugins/ui.lua
+++ config/nvim/lua/plugins/ui.lua
@@ -1,56 +1,52 @@
 return {
-	{
-		"nvim-tree/nvim-web-devicons",
-	},
-	{
-		"stevearc/dressing.nvim",
-		opts = {
-			input = { default_prompt = "➤ " },
-			select = { backend = { "telescope", "builtin", "nui" } },
-		},
-	},
-	{
-		"NvChad/nvim-colorizer.lua",
-		cmd = {
-			"ColorizerToggle",
-			"ColorizerAttachToBuffer",
-			"ColorizerDetachFromBuffer",
-			"ColorizerReloadAllBuffers",
-		},
-		opts = { user_default_options = { names = false } },
-	},
-	{
-		"lukas-reineke/indent-blankline.nvim",
-		event = "VeryLazy",
-		config = function()
-			require("indent_blankline").setup({
-				-- for example, context is off by default, use this to turn it on
-				show_current_context = true,
-				show_current_context_start = true,
-				show_end_of_line = true,
-			})
-		end,
-	},
-	{
-		"folke/noice.nvim",
-		event = "VeryLazy",
-		opts = {
-			-- add any options here
-		},
-		dependencies = {
-			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-			"MunifTanjim/nui.nvim",
-			-- OPTIONAL:
-			--   `nvim-notify` is only needed, if you want to use the notification view.
-			--   If not available, we use `mini` as the fallback
-			{
-				"rcarriga/nvim-notify",
-				config = function()
-					require("notify").setup({
-						timeout = 300,
-					})
-				end,
-			},
-		},
-	},
+  {
+    "nvim-tree/nvim-web-devicons",
+  },
+  {
+    "stevearc/dressing.nvim",
+    opts = {
+      input = { default_prompt = "➤ " },
+      select = { backend = { "telescope", "builtin", "nui" } },
+    },
+  },
+  {
+    "NvChad/nvim-colorizer.lua",
+    cmd = {
+      "ColorizerToggle",
+      "ColorizerAttachToBuffer",
+      "ColorizerDetachFromBuffer",
+      "ColorizerReloadAllBuffers",
+    },
+    opts = { user_default_options = { names = false } },
+  },
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("indent_blankline").setup({
+        -- for example, context is off by default, use this to turn it on
+        show_current_context = true,
+        show_current_context_start = true,
+        show_end_of_line = true,
+      })
+    end,
+  },
+  {
+    "rcarriga/nvim-notify",
+    config = function()
+      require("notify").setup({
+        timeout = 5,
+      })
+    end,
+  },
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- add any options here
+    },
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+    },
+  },
 }
