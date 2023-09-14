36f239a67897f2d26723a2a1b47657fc4ab84b9d
hnpyn <wangmingne@outlook.com>
2023-09-14 23:54:19 +0800

    update noice.nvim

--- config/nvim/lua/plugins/ui.lua
+++ config/nvim/lua/plugins/ui.lua
@@ -87,11 +87,17 @@ return {
 		enabled = true,
 		event = "VeryLazy",
 		opts = {
+			cmdline = {
+				view = "cmdline_popup",
+			},
 			lsp = {
 				override = {
 					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
 					["vim.lsp.util.stylize_markdown"] = true,
 					["cmp.entry.get_documentation"] = true,
+				},
+				progress = {
+					enabled = false,
 				},
 			},
 			routes = {
@@ -111,7 +117,8 @@ return {
 				bottom_search = true,
 				command_palette = true,
 				long_message_to_split = true,
-				inc_rename = true,
+				inc_rename = false,
+				lsp_doc_border = false,
 			},
 		},
     -- stylua: ignore
