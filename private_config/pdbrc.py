22c008efea53c7310afb2691f381f4871bac92cb
hnpyn <wangmingne@outlook.com>
2023-09-23 12:09:51 +0800

    update private config

--- private_config/pdbrc.py
+++ private_config/pdbrc.py
@@ -12,7 +12,7 @@ try:
         use_pygments = True
         sticky_by_default = True
         prompt = "\033[1;92mpdb++> \033[0m"
-        current_line_color = "39;100;1"
+        current_line_color = "1;100;1"
 
 except ModuleNotFoundError:
     pass
