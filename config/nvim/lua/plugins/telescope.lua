f924b7027ecd287394dd6aad4b1484821230f699
hnpyn <wangmingne@outlook.com>
2024-01-22 15:17:20 +0800

    update nvim telescope

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -1,29 +1,29 @@
 return {
-  {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.1",
-    cmd = { "Telescope" },
-    keys = {
-      { "<Leader>ff", "<Cmd>Telescope find_files<CR>", desc = "Telescope: find files" },
-      { "<Leader>fg", "<Cmd>Telescope live_grep<CR>",  desc = "Telescope: live grep" },
-      { "<Leader>fb", "<Cmd>Telescope buffers<CR>",    desc = "Telescope: buffers" },
-      { "<Leader>fr", "<Cmd>Telescope resume<CR>",     desc = "Telescope: resume" },
-      { "<Leader>fo", "<Cmd>Telescope oldfiles<CR>",   desc = "Telescope: old files" },
-    },
-    opts = {
-      defaults = {
-        sorting_strategy = "ascending",
-        layout_config = {
-          prompt_position = "top",
-        },
-        mappings = {
-          n = {
-            ["q"] = function(...)
-              return require("telescope.actions").close(...)
-            end,
-          },
-        },
-      },
-    },
-  },
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.4",
+		cmd = { "Telescope" },
+		keys = {
+			{ "<Leader>ff", "<Cmd>Telescope find_files<CR>", desc = "Telescope: find files" },
+			{ "<Leader>fg", "<Cmd>Telescope live_grep<CR>", desc = "Telescope: live grep" },
+			{ "<Leader>fb", "<Cmd>Telescope buffers<CR>", desc = "Telescope: buffers" },
+			{ "<Leader>fr", "<Cmd>Telescope resume<CR>", desc = "Telescope: resume" },
+			{ "<Leader>fo", "<Cmd>Telescope oldfiles<CR>", desc = "Telescope: old files" },
+		},
+		opts = {
+			defaults = {
+				sorting_strategy = "ascending",
+				layout_config = {
+					prompt_position = "top",
+				},
+				mappings = {
+					n = {
+						["q"] = function(...)
+							return require("telescope.actions").close(...)
+						end,
+					},
+				},
+			},
+		},
+	},
 }
