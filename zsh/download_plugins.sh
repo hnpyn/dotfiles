65a31c409d7b030cec166913a8bec337c797af5d
hnpyn <wangmingne@outlook.com>
2024-02-08 14:32:47 +0800

    update zsh configs

--- zsh/download_plugins.sh
+++ zsh/download_plugins.sh
@@ -12,9 +12,13 @@ rm -rf $ZSH/plugins
 mkdir -p $ZSH/plugins
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH/plugins/powerlevel10k
+echo ''
 git clone https://github.com/Aloxaf/fzf-tab $ZSH/plugins/fzf-tab
+echo ''
 git clone https://github.com/zdharma-continuum/fast-syntax-highlighting $ZSH/plugins/fast-syntax-highlighting
+echo ''
 git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH/plugins/zsh-autosuggestions
+echo ''
 git clone https://github.com/jeffreytse/zsh-vi-mode.git $ZSH/plugins/zsh-vi-mode
 
 echo ''
