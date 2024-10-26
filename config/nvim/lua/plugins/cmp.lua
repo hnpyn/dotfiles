aed7c433af58cd5f39340faef7489eea46068c06
hnpyn <wangmingne@outlook.com>
2024-10-26 23:51:01 +0800

    Update nvim cmp -- add codeium

--- config/nvim/lua/plugins/cmp.lua
+++ config/nvim/lua/plugins/cmp.lua
@@ -10,10 +10,16 @@ return {
 		event = "InsertEnter",
 		dependencies = {
 			"L3MON4D3/LuaSnip",
+			"saadparwaiz1/cmp_luasnip",
 			"hrsh7th/cmp-nvim-lsp",
 			"hrsh7th/cmp-buffer",
 			"hrsh7th/cmp-path",
 			"hrsh7th/cmp-cmdline",
+			{
+				"Exafunction/codeium.nvim",
+				cmd = "Codeium",
+				opts = {},
+			},
 		},
 		config = function()
 			local cmp = require("cmp")
@@ -65,6 +71,7 @@ return {
 					end, { "i", "s" }),
 				}),
 				sources = cmp.config.sources({
+					{ name = "codeium" },
 					{ name = "nvim_lsp" },
 					{ name = "luasnip" }, -- For luasnip users.
 					-- { name = "ultisnips" }, -- For ultisnips users.
