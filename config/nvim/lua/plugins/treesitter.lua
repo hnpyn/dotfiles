993b8bafa1206ebe844734bcaa2618931fc8d2c9
hnpyn <wangmingne@outlook.com>
2025-05-18 20:21:54 +0800

    Update nvim config

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -1,7 +1,9 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
-		event = { "BufReadPost", "BufNewFile" },
+		event = { "BufReadPost", "BufNewFile", "VeryLazy" },
+		lazy = vim.fn.argc(-1) == 0,
+		cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
 		config = function()
 			-- change highlight
 			-- vim.api.nvim_set_hl(0, "@lsp.type.variable.lua", { link = "Normal" })
@@ -41,82 +43,8 @@ return {
 	},
 	{
 		"nvim-treesitter/nvim-treesitter-textobjects",
-		init = function()
-			-- disable rtp plugin, as we only need its queries for mini.ai
-			-- In case other textobject modules are enabled, we will load them
-			-- once nvim-treesitter is loaded
-			require("lazy.core.loader").disable_rtp_plugin("nvim-treesitter-textobjects")
-			load_textobjects = true
-		end,
 		event = "VeryLazy",
-	},
-	{
-		"echasnovski/mini.ai",
-		-- keys = {
-		--   { "a", mode = { "x", "o" } },
-		--   { "i", mode = { "x", "o" } },
-		-- },
-		event = "VeryLazy",
-		dependencies = { "nvim-treesitter-textobjects" },
-		opts = function()
-			local ai = require("mini.ai")
-			return {
-				n_lines = 500,
-				custom_textobjects = {
-					o = ai.gen_spec.treesitter({
-						a = { "@block.outer", "@conditional.outer", "@loop.outer" },
-						i = { "@block.inner", "@conditional.inner", "@loop.inner" },
-					}, {}),
-					f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }, {}),
-					c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }, {}),
-				},
-			}
-		end,
-		config = function(_, opts)
-			require("mini.ai").setup(opts)
-			-- register all text objects with which-key
-			require("utils").on_load("which-key.nvim", function()
-				---@type table<string, string|table>
-				local i = {
-					[" "] = "Whitespace",
-					['"'] = 'Balanced "',
-					["'"] = "Balanced '",
-					["`"] = "Balanced `",
-					["("] = "Balanced (",
-					[")"] = "Balanced ) including white-space",
-					[">"] = "Balanced > including white-space",
-					["<lt>"] = "Balanced <",
-					["]"] = "Balanced ] including white-space",
-					["["] = "Balanced [",
-					["}"] = "Balanced } including white-space",
-					["{"] = "Balanced {",
-					["?"] = "User Prompt",
-					_ = "Underscore",
-					a = "Argument",
-					b = "Balanced ), ], }",
-					c = "Class",
-					f = "Function",
-					o = "Block, conditional, loop",
-					q = "Quote `, \", '",
-					t = "Tag",
-				}
-				local a = vim.deepcopy(i)
-				for k, v in pairs(a) do
-					a[k] = v:gsub(" including.*", "")
-				end
-
-				local ic = vim.deepcopy(i)
-				local ac = vim.deepcopy(a)
-				for key, name in pairs({ n = "Next", l = "Last" }) do
-					i[key] = vim.tbl_extend("force", { name = "Inside " .. name .. " textobject" }, ic)
-					a[key] = vim.tbl_extend("force", { name = "Around " .. name .. " textobject" }, ac)
-				end
-				require("which-key").register({
-					mode = { "o", "x" },
-					i = i,
-					a = a,
-				})
-			end)
-		end,
+		enabled = true,
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
 	},
 }
