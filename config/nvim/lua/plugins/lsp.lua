180fadec60fc7d86a692f22b76e101009e830123
hnpyn <wangmingne@outlook.com>
2025-05-20 15:53:48 +0800

    Update nvim config

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -1,133 +1,70 @@
 return {
-	{ "folke/neodev.nvim", opts = {} },
-	{
-		"neovim/nvim-lspconfig",
-		event = { "BufReadPost", "BufNewFile" },
-	},
-	{ "williamboman/mason-lspconfig.nvim" },
-	{
-		"williamboman/mason.nvim",
-		cmd = {
-			"Mason",
-			"MasonInstall",
-			"MasonUninstall",
-			"MasonUninstallAll",
-			"MasonLog",
-			"MasonUpdate",
-			"MasonUpdateAll",
-		},
-		opts = {
-			ensure_installed = {
-				"stylua",
-				"shfmt",
-				"black",
-				"python-lsp-server",
-			},
-			ui = {
-				icons = {
-					package_installed = "✓",
-					package_uninstalled = "✗",
-					package_pending = "⟳",
-				},
-			},
-		},
-		build = ":MasonUpdate", -- :MasonUpdate updates registry contents
-	},
-	{
-		"nvimtools/none-ls.nvim",
-		event = { "BufReadPost", "BufNewFile" },
-		config = function()
-			local null_ls = require("null-ls")
-			local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+  { "folke/neodev.nvim",                opts = {} },
+  {
+    "neovim/nvim-lspconfig",
+    event = { "BufReadPost", "BufNewFile" },
+  },
+  { "williamboman/mason-lspconfig.nvim" },
+  {
+    "williamboman/mason.nvim",
+    cmd = {
+      "Mason",
+      "MasonInstall",
+      "MasonUninstall",
+      "MasonUninstallAll",
+      "MasonLog",
+      "MasonUpdate",
+      "MasonUpdateAll",
+    },
+    opts = {
+      ensure_installed = {
+        "stylua",
+        "shfmt",
+        "black",
+        "python-lsp-server",
+      },
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_uninstalled = "✗",
+          package_pending = "⟳",
+        },
+      },
+    },
+    build = ":MasonUpdate", -- :MasonUpdate updates registry contents
+  },
+  {
+    "nvimtools/none-ls.nvim",
+    event = { "BufReadPost", "BufNewFile" },
+    config = function()
+      local null_ls = require("null-ls")
+      local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
-			null_ls.setup({
-				sources = {
-					null_ls.builtins.formatting.stylua,
-					null_ls.builtins.formatting.black.with({
-						extra_args = { "--line-length", "120" },
-					}),
-					null_ls.builtins.formatting.clang_format,
-					null_ls.builtins.formatting.markdownlint,
-					-- null_ls.builtins.formatting.yamlfmt,
-					null_ls.builtins.formatting.shfmt,
-				},
-				on_attach = function(client, bufnr)
-					if client.supports_method("textDocument/formatting") then
-						vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-						vim.api.nvim_create_autocmd("BufWritePre", {
-							group = augroup,
-							buffer = bufnr,
-							callback = function()
-								-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-								vim.lsp.buf.format({ bufnr = bufnr })
-							end,
-						})
-					end
-				end,
-			})
-		end,
-	},
-	{
-		"stevearc/aerial.nvim",
-		enabled = false,
-		config = function()
-			require("aerial").setup({
-				attach_mode = "global",
-				backends = { "lsp", "treesitter", "markdown", "man" },
-				disable_max_lines = 1000,
-				disable_max_size = 102400,
-				layout = { min_width = 28 },
-				show_guides = true,
-				filter_kind = false,
-				guides = {
-					mid_item = "├ ",
-					last_item = "└ ",
-					nested_top = "│ ",
-					whitespace = "  ",
-				},
-				keymaps = {
-					["[y"] = "actions.prev",
-					["]y"] = "actions.next",
-					["[Y"] = "actions.prev_up",
-					["]Y"] = "actions.next_up",
-					["{"] = false,
-					["}"] = false,
-					["[["] = false,
-					["]]"] = false,
-				},
-			})
-			vim.keymap.set("n", "<Leader>a", "<Cmd>AerialToggle!<CR>")
-		end,
-	},
-	{
-		"SmiteshP/nvim-navic",
-		enabled = false,
-		lazy = true,
-		init = function()
-			-- vim.o.winbar = "%{%v:lua.require'nvim-navic'.get_location()%}"
-			vim.g.navic_silence = true
-			require("utils").on_attach(function(client, buffer)
-				if client.server_capabilities.documentSymbolProvider then
-					require("nvim-navic").attach(client, buffer)
-				end
-			end)
-		end,
-		opts = function()
-			return {
-				separator = " ",
-				highlight = true,
-				depth_limit = 5,
-				icons = require("config.ui").icons.kinds,
-			}
-		end,
-	},
-	{
-		"SmiteshP/nvim-navbuddy",
-		enabled = false,
-		cmd = "Navbuddy",
-		init = function()
-			vim.cmd.cnoreabbrev("na Navbuddy")
-		end,
-		opts = { lsp = { auto_attach = true } },
-	},
+      null_ls.setup({
+        sources = {
+          null_ls.builtins.formatting.stylua,
+          null_ls.builtins.formatting.black.with({
+            extra_args = { "--line-length", "120" },
+          }),
+          null_ls.builtins.formatting.clang_format,
+          null_ls.builtins.formatting.markdownlint,
+          -- null_ls.builtins.formatting.yamlfmt,
+          null_ls.builtins.formatting.shfmt,
+        },
+        on_attach = function(client, bufnr)
+          if client.supports_method("textDocument/formatting") then
+            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+            vim.api.nvim_create_autocmd("BufWritePre", {
+              group = augroup,
+              buffer = bufnr,
+              callback = function()
+                -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+                vim.lsp.buf.format({ bufnr = bufnr })
+              end,
+            })
+          end
+        end,
+      })
+    end,
+  },
 }
--- config/nvim/lua/plugins/ui.lua
+++ config/nvim/lua/plugins/ui.lua
@@ -1,195 +1,69 @@
 return {
-	{ "nvim-tree/nvim-web-devicons", lazy = true },
-	{ "MunifTanjim/nui.nvim", lazy = true },
-	{
-		"stevearc/dressing.nvim",
-		lazy = true,
-		init = function()
-			---@diagnostic disable-next-line: duplicate-set-field
-			vim.ui.select = function(...)
-				require("lazy").load({ plugins = { "dressing.nvim" } })
-				return vim.ui.select(...)
-			end
-			---@diagnostic disable-next-line: duplicate-set-field
-			vim.ui.input = function(...)
-				require("lazy").load({ plugins = { "dressing.nvim" } })
-				return vim.ui.input(...)
-			end
-		end,
-	},
-	{
-		"NvChad/nvim-colorizer.lua",
-		cmd = {
-			"ColorizerToggle",
-			"ColorizerAttachToBuffer",
-			"ColorizerDetachFromBuffer",
-			"ColorizerReloadAllBuffers",
-		},
-		opts = { user_default_options = { names = false } },
-	},
-	{
-		"lukas-reineke/indent-blankline.nvim",
-		event = { "BufReadPost", "BufNewFile" },
-		opts = {
-			indent = {
-				char = "│",
-				tab_char = "│", -- ▏
-			},
-			scope = { enabled = false },
-			exclude = {
-				filetypes = {
-					"help",
-					"alpha",
-					"dashboard",
-					"neo-tree",
-					"Trouble",
-					"lazy",
-					"mason",
-					"notify",
-					"toggleterm",
-					"lazyterm",
-				},
-			},
-		},
-		main = "ibl",
-	},
-	{
-		"rcarriga/nvim-notify",
-		enabled = false,
-		keys = {
-			{
-				"<leader>un",
-				function()
-					require("notify").dismiss({ silent = true, pending = true })
-				end,
-				desc = "Dismiss all Notifications",
-			},
-		},
-		opts = {
-			timeout = 2000,
-			max_height = function()
-				return math.floor(vim.o.lines * 0.75)
-			end,
-			max_width = function()
-				return math.floor(vim.o.columns * 0.75)
-			end,
-		},
-		init = function()
-			-- when noice is not enabled, install notify on VeryLazy
-			local Util = require("utils")
-			if not Util.has("noice.nvim") then
-				Util.on_very_lazy(function()
-					vim.notify = require("notify")
-				end)
-			end
-		end,
-	},
-	{
-		"folke/noice.nvim",
-		enabled = false,
-		event = "VeryLazy",
-		opts = {
-			cmdline = {
-				view = "cmdline",
-				format = {
-					cmdline = { pattern = "^:", icon = "❯", lang = "vim" },
-				},
-			},
-			notify = {
-				enabled = false,
-			},
-			lsp = {
-				override = {
-					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-					["vim.lsp.util.stylize_markdown"] = true,
-					["cmp.entry.get_documentation"] = true,
-				},
-				progress = {
-					enabled = false,
-				},
-			},
-			routes = {
-				{
-					filter = {
-						event = "msg_show",
-						any = {
-							{ find = "%d+L, %d+B" },
-							{ find = "; after #%d+" },
-							{ find = "; before #%d+" },
-						},
-					},
-					view = "mini",
-				},
-			},
-			presets = {
-				bottom_search = true,
-				command_palette = false,
-				long_message_to_split = true,
-				inc_rename = false,
-				lsp_doc_border = false,
-			},
-		},
-    -- stylua: ignore
-    keys = {
-      {
-        "<S-Enter>",
-        function() require("noice").redirect(vim.fn.getcmdline()) end,
-        mode = "c",
-        desc =
-        "Redirect Cmdline"
+  { "nvim-tree/nvim-web-devicons", lazy = true },
+  { "MunifTanjim/nui.nvim",        lazy = true },
+  {
+    "stevearc/dressing.nvim",
+    lazy = true,
+    init = function()
+      ---@diagnostic disable-next-line: duplicate-set-field
+      vim.ui.select = function(...)
+        require("lazy").load({ plugins = { "dressing.nvim" } })
+        return vim.ui.select(...)
+      end
+      ---@diagnostic disable-next-line: duplicate-set-field
+      vim.ui.input = function(...)
+        require("lazy").load({ plugins = { "dressing.nvim" } })
+        return vim.ui.input(...)
+      end
+    end,
+  },
+  {
+    "NvChad/nvim-colorizer.lua",
+    cmd = {
+      "ColorizerToggle",
+      "ColorizerAttachToBuffer",
+      "ColorizerDetachFromBuffer",
+      "ColorizerReloadAllBuffers",
+    },
+    opts = { user_default_options = { names = false } },
+  },
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    event = { "BufReadPost", "BufNewFile" },
+    opts = {
+      indent = {
+        char = "│",
+        tab_char = "│", -- ▏
       },
-      {
-        "<leader>nl",
-        function() require("noice").cmd("last") end,
-        desc =
-        "Noice Last Message"
-      },
-      {
-        "<leader>nh",
-        function() require("noice").cmd("history") end,
-        desc =
-        "Noice History"
-      },
-      { "<leader>sna", function() require("noice").cmd("all") end, desc = "Noice All" },
-      {
-        "<leader>nd",
-        function() require("noice").cmd("dismiss") end,
-        desc =
-        "Dismiss All"
-      },
-      {
-        "<c-f>",
-        function() if not require("noice.lsp").scroll(4) then return "<c-f>" end end,
-        silent = true,
-        expr = true,
-        desc =
-        "Scroll forward",
-        mode = {
-          "i", "n", "s" }
-      },
-      {
-        "<c-b>",
-        function() if not require("noice.lsp").scroll(-4) then return "<c-b>" end end,
-        silent = true,
-        expr = true,
-        desc =
-        "Scroll backward",
-        mode = {
-          "i", "n", "s" }
+      scope = { enabled = false },
+      exclude = {
+        filetypes = {
+          "help",
+          "alpha",
+          "dashboard",
+          "neo-tree",
+          "Trouble",
+          "lazy",
+          "mason",
+          "notify",
+          "toggleterm",
+          "lazyterm",
+        },
       },
     },
-	},
-	{
-		"petertriho/nvim-scrollbar",
-		enabled = true,
-		event = { "BufReadPost", "BufNewFile" },
-		config = function()
-			require("scrollbar").setup()
-		end,
-	},
-	{
-		"dstein64/nvim-scrollview",
-		enabled = false,
-		event = { "BufReadPost", "BufNewFile" },
-	},
+    main = "ibl",
+  },
+  {
+    "petertriho/nvim-scrollbar",
+    enabled = true,
+    event = { "BufReadPost", "BufNewFile" },
+    config = function()
+      require("scrollbar").setup()
+    end,
+  },
+  {
+    "dstein64/nvim-scrollview",
+    enabled = false,
+    event = { "BufReadPost", "BufNewFile" },
+  },
 }
