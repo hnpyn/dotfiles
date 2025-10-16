a77ba46bace88c2d21921b80ca83b4dfd9a57178
hnpyn <wangmingne@outlook.com>
2025-10-16 23:06:25 +0800

    Update

--- config/nvim/lua/plugins/editor.lua
+++ config/nvim/lua/plugins/editor.lua
@@ -226,6 +226,10 @@ return {
 				sources = { "filesystem", "buffers", "git_status", "document_symbols" },
 				open_files_do_not_replace_types = { "terminal", "Trouble", "qf", "Outline" },
 				filesystem = {
+					filtered_items = {
+						hide_dotfiles = false,
+						hide_gitignored = false,
+					},
 					bind_to_cwd = false,
 					follow_current_file = { enabled = true },
 					use_libuv_file_watcher = true,
